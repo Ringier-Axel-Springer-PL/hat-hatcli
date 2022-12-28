f63985ec552bbb8ae97d5052959786a32e10f7e5
Arkadiusz <arkadiusz.fus@quantoo.pl>
2022-12-28 16:11:07 +0100

    WIP

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,19 @@
 #! /usr/bin/env node
-console.log("Hello, World!");
+'use strict';
+process.on('unhandledRejection', err => {
+    throw err;
+});
+
+const args = process.argv.slice(2);
+
+const scriptIndex = args.findIndex(
+    x => x === 'build' || x === 'eject' || x === 'start' || x === 'test'
+);
+const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
+const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
+
+if (['setup'].includes(script)) {
+    console.log('Work in progress...')
+} else {
+    console.log('Unknown script "' + script + '".');
+}
