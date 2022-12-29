a0a3d22b0fd60cdaf06cddc807a620bb3a77e131
Arkadiusz <arkadiusz.fus@quantoo.pl>
2022-12-29 15:13:36 +0100

    0.0.3

--- bin/index.js
+++ bin/index.js
@@ -12,6 +12,7 @@ const scriptIndex = args.findIndex(
 );
 const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
 const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
+
 async function handleScript(script) {
     const scriptPath = './../lib/scripts/';
 
