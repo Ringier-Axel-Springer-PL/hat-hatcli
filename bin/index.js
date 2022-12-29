7c59de2adc0f64faaa35f3d906c12b0515a2fb6f
Arkadiusz <arkadiusz.fus@quantoo.pl>
2022-12-29 14:34:48 +0100

    0.0.2

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+
 'use strict';
 process.on('unhandledRejection', err => {
     throw err;
@@ -11,9 +12,22 @@ const scriptIndex = args.findIndex(
 );
 const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
 const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
+async function handleScript(script) {
+    const scriptPath = './../lib/scripts/';
 
-if (['setup'].includes(script)) {
-    console.log('Work in progress...')
+    try {
+        const ScriptClass = require(`${scriptPath}${script}`);
+        const scriptInstance = new ScriptClass();
+        await scriptInstance.execute();
+
+    } catch (error) {
+        console.error(error.stack || error.message || error);
+        process.exit(1); // exit with error status
+    }
+}
+
+if (['setup', 'start'].includes(script)) {
+    handleScript(script);
 } else {
     console.log('Unknown script "' + script + '".');
 }
